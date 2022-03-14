886251a6cc9e9b5961c17dfe0e96c1137fdee1eb
squareyun <squareyun@gmail.com>
2022-03-14 22:36:28 +0900

    20220314

---
+++ ndb_book/implementation/상하좌우.py
@@ -0,0 +1,18 @@
+n = int(input())
+plans = input().split()
+x, y = 1, 1
+
+dir = ['L', 'R', 'U', 'D']
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+for plan in plans:
+    for i in range(len(dir)):
+        if plan == dir[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or nx > n or ny < 1 or ny > n:
+        continue
+    x, y = nx, ny
+
+print(x, y)
