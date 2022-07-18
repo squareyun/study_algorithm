ca7b92efbcef07ad254171299fd21d26c4bdf120
squareyun <squareyun@gmail.com>
2022-07-19 01:02:37 +0900

    20220718

--- baekjoon/16500.py
+++ baekjoon/16500.py
@@ -1,19 +1,36 @@
+# 직관적인 풀이
 s = input()
 n = int(input())
-a = []
-for i in range(n):
-    a.append(input())
-dp = [-1] * (len(s) + 1)
+a = [input() for i in range(n)]
+dp = [False for _ in range(len(s) + 1)]
 
-def word(pos):
-    if dp[pos] != -1: return dp[pos]
+dp[0] = True
+for i in range(len(s)):
+    if not dp[i]:
+        continue
 
-    if pos == len(s): return 1
+    for t in a:
+        if s[i:i+len(t)] == t:
+            dp[i+len(t)] = True
+print(int(dp[len(s)]))
 
-    dp[pos] = 0
-    for i in range(n):
-        if s[pos:].startswith(a[i]):
-            dp[pos] = max(dp[pos], word(pos + len(a[i])))
-    return dp[pos]
+# 다른 풀이
+# s = input()
+# n = int(input())
+# a = []
+# for i in range(n):
+#     a.append(input())
+# dp = [-1] * (len(s) + 1)
 
-print(word(0))
+# def word(pos):
+#     if dp[pos] != -1: return dp[pos]
+
+#     if pos == len(s): return 1
+
+#     dp[pos] = 0
+#     for i in range(n):
+#         if s[pos:].startswith(a[i]):
+#             dp[pos] = max(dp[pos], word(pos + len(a[i])))
+#     return dp[pos]
+
+# print(word(0))
