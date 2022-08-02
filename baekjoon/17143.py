be36893dfeafd4ed59191ea947a225b28a122793
squareyun <squareyun@gmail.com>
2022-08-02 14:43:37 +0900

    solve boj 17143 낚시왕

--- baekjoon/17143.py
+++ baekjoon/17143.py
@@ -31,57 +31,28 @@ def move():
             if not shark[r][c]: continue
             sh=shark[r][c]
             s,d,z=sh[0],sh[1],sh[2]
-            nx=r+s*dx[d]
-            ny=c+s*dy[d]
-            if nx < 1 or ny < 1 or nx > R or ny > C:
-                if d == 1:
-                    t = s - (r-1)
-                    dir,move=divmod(t,(R-1))
-                    if r != 1: d = change(d)
-                    if dir % 2 == 0:
-                        nx=1+move
-                        if dir != 0 :
-                            d = change(d)
-                    else:
-                        nx=R-move
-                        if move != 0:
-                            d = change(d)
-                elif d == 2:
-                    t = s - (R-r)
-                    dir,move=divmod(t,(R-1))
-                    if r != R: d = change(d)
-                    if dir % 2 == 0:
-                        nx=R-move
-                        if dir != 0 :
-                            d = change(d)
-                    else:
-                        nx=1+move
-                        if move != 0:
-                            d = change(d)
-                elif d == 3:
-                    t = s - (C-c)
-                    dir,move=divmod(t,(C-1))
-                    if c != C: d = change(d)
-                    if dir % 2 == 0:
-                        ny=C-move
-                        if dir != 0 :
-                            d = change(d)
-                    else:
-                        ny=1+move
-                        if move != 0:
-                            d = change(d)
-                elif d == 4:
-                    t = s - (c-1)
-                    dir,move=divmod(t,(C-1))
-                    if c != 1: d = change(d)
-                    if dir % 2 == 0:
-                        ny=1+move
-                        if dir != 0 :
-                            d = change(d)
-                    else:
-                        ny=C-move
-                        if move != 0:
-                            d = change(d)
+            nx,ny=r,c
+
+            if d==1 or d==2:
+                s=s%(2*R-2)
+            elif d==3 or d==4:
+                s=s%(2*C-2)
+
+            for i in range(s):
+                nx=nx+dx[d]
+                ny=ny+dy[d]
+                if nx < 1:
+                    d=change(d)
+                    nx+=2
+                elif nx > R:
+                    d=change(d)
+                    nx-=2
+                elif ny < 1:
+                    d=change(d)
+                    ny+=2
+                elif ny > C:
+                    d=change(d)
+                    ny-=2
 
             if not tshark[nx][ny] or (z > tshark[nx][ny][2]):
                 tshark[nx][ny] = [s,d,z]
