0237808275cbae172243f8ebbe06e09a74b7aa69
squareyun <squareyun@gmail.com>
2022-08-09 00:25:40 +0900

    solved 20220808

--- programmers/순위검색.py
+++ programmers/순위검색.py
@@ -5,53 +5,43 @@
 # AUTHOR: squareyun
 
 def searchindex(score, target):
-    l = 0; r = len(score)
-    while True:
+    l = 0; r = len(score)-1
+    tmp = len(score)
+    while l <= r:
         m = (l+r)//2
-        mNum = score[m][1]
-        if mNum == target:
-            while score[m][1] == target:
-                m-=1
-            return m+1
-        elif abs(l-r)<=1:
-            return m+1
-
-        if mNum > target:
-            r = m - 1
+        if target <= score[m]:
+            tmp = m
+            r=m-1
         else:
-            l = m          
+            l=m+1
+    return tmp
 
 def solution(info, query):
-    answer = []; data=[]; score=[]
-    for i in range(len(info)):
-        t=info[i].split()
-        data.append(t[:-1])
-        score.append((i, int(t[-1])))
+    answer=[]
+    hash={}
+    for i in ['cpp','java','python','-']:
+        for j in ['backend','frontend','-']:
+            for k in ['junior','senior','-']:
+                for l in ['chicken','pizza','-']:
+                    hash[i+j+k+l] =  []
     
-    score.sort(key=lambda x:x[1])
+    for infom in info:
+        infom = infom.split(" ")
+        for i in [infom[0], '-']:
+            for j in [infom[1], '-']:
+                for k in [infom[2], '-']:
+                    for l in [infom[3], '-']:
+                        hash[i+j+k+l].append(int(infom[4]))
     
-    for i in range(len(query)):
-        target=query[i].split(" and ")
-        t = target[-1].split(" ")
-        target[-1] = t[0]
-        tscore = int(t[1])
-        
-        sidx = searchindex(score, tscore)
-        searchidx=set()
-        for j in range(sidx,len(score)):
-            searchidx.add(score[j][0])
-        
-        tsearchidx=searchidx.copy()
-        for j in range(4):
-            if target[j] == '-': continue
-            for k in searchidx:
-                if k not in tsearchidx: continue
-                if data[k][j] != target[j]:
-                    tsearchidx.remove(k)
-        answer.append(len(tsearchidx))
-        
-    return answer
+    for key in hash.keys():
+        hash[key].sort()
+    
+    for sql in query:
+        sql = sql.replace(" and ", "")
+        sql = sql.split()
+        target = int(sql[1])
+        sql = sql[0]
+        ret = searchindex(hash[sql], target)
+        answer.append(len(hash[sql]) - ret)
 
-info=["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"]
-query=["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]
-print(solution(info,query))
+    return answer
