3e8d265a5ebbb5adca5d2dba0b6b1b370fb41164
윤우혁 <squareyun@gmail.com>
2023-03-04 15:17:57 +0900

    20230304. bfs, simulation

---
+++ baekjoon/16974_아기상어.java
@@ -0,0 +1,28 @@
+//import java.util.Scanner;
+//
+//public class BOJ_16974 {
+//
+//    static int N, X;
+//    static long[] burn;
+//    static long[] patty;
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//        N = sc.nextInt();
+//        X = sc.nextInt();
+//
+//        burn = new long[N+1];
+//        patty = new long[N+1];
+//
+//        dfs(N);
+//    }
+//
+//    void dfs(int level) {
+//        if (level == 0) {
+//
+//        }
+//
+//        dfs(level + 1);
+//    }
+//
+//}
